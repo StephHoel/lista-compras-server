328705edcf66948278cdb01a79203afaee3a79ce
Steph Hoel <steph.hoel@gmail.com>
2023-10-14 10:37:27 -0300

    feat: initial config

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import cors from '@fastify/cors'
+import Fastify from 'fastify'
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  await fastify.listen({ 
+    port: 3333, 
+    host: '0.0.0.0' 
+  })
+}
+
+bootstrap()
